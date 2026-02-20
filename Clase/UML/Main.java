43aee2c6825ecca3579e87215893dc8480f3562f
Juan <email>
2026-02-19 22:21:10 -0500

    Corrección de llamado al método getNombre(), y cambio de nombre del paquete "UML"

--- Clase/UML/Main.java
+++ Clase/UML/Main.java
@@ -1,23 +1,23 @@
-package Uml;
+package UML;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        Cicla miBici = new Uml.Cicla("C-01", "todoterreno", true, "negro", 26 );
+        Cicla miBici = new Cicla("C-01", "todoterreno", true, "negro", 26 );
 
         JOptionPane.showMessageDialog(null, miBici);
         miBici.setNombre("UnaRueda");
-        System.out.println(miBici.getNombre);
+        miBici.setId("C-02");
+        miBici.setFreno(false);
+        miBici.setColor("Azul");
+        miBici.setTallaRin(25);
+        JOptionPane.showMessageDialog(null, miBici);
 
 
 
         miBici.acelerar(); //llamado de metodo NO static
         //Cicla.acelerar(); //llamado de metodo static
-        System.out.println(miBici.getId);
-        miBici.setId("C-02");
-        System.out.println(miBici.getId);
-
 
     }
 }
